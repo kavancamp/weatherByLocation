5580a2af2e957b233f28508344883bacfc30d31b
Keenah <kvancamp@mail.com>
2023-05-01 12:14:38 -0500

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -15,66 +15,33 @@ function changeCity(event) {
 }
 
 let cityButton = document.querySelector("#search-form");
-cityButton.addEventListener("click", changeCity);
+cityButton.addEventListener("submit", changeCity);
 
 //get & display time
 window.onload = function () {
   let now = new Date();
-  let date = now.getDate();
-  let year = now.getFullYear();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let month = months[now.getMonth()];
-
-  let today = `Today is ${day}, ${month} ${date}, ${year}`;
-  document.getElementById("date").innerHTML = today;
+  let todaysDate = now.toLocaleDateString();
+  var time = now.getHours() + ":" + now.getMinutes();
+  document.getElementById(`date`).innerHTML = `${todaysDate} ${time}`;
 };
 
 //get location & display when current is clicked
 
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
 function showLocation(position) {
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let api_key = "df105d3db4bbc2adb0adee0461f34a46";
-
   let endPoint = "https://api.openweathermap.org/data/2.5/weather?";
   let url = `${endPoint}lat=${lat}&lon=${long}&appid=${api_key}&units=imperial`;
-
   console.log(url);
   console.log(`lat: ${lat}, long: ${long}`);
-  console.log();
   axios.get(`${url}`).then(showTemp);
 }
 
-function getLocation() {
-  navigator.geolocation.getCurrentPosition(showLocation);
-}
-
 function showTemp(response) {
-  console.log(response.data);
-  console.log(response.data.weather[0].icon);
   let name = response.data.name;
   let country = response.data.sys.country;
   let temp = Math.round(response.data.main.temp);
@@ -83,16 +50,34 @@ function showTemp(response) {
   let description = response.data.weather[0].description;
   let icon = response.data.weather[0].icon;
   let speed = response.data.wind.speed;
-  let source = "https://openweathermap.org/img/wn/";
-
+  // prettier-ignore
+  let celTemp = Math.round((temp - 32) * 5 / 9);
+  console.log(temp, celTemp);
   document.querySelector("h1").innerHTML = `${name}, ${country}`;
-  document.querySelector(".icon").src.innerHTML = `${source}${icon}.png`;
   document.querySelector(".feels_like").innerHTML = `${feel}°`;
   document.querySelector(".description").innerHTML = `${description}`;
   document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`;
-  document.querySelector(".temp").innerHTML = `${temp} °F`;
+  document.querySelector(".temp").innerHTML = `${temp}`;
+  document.querySelector(".temp-celsius").innerHTML = `${celTemp}`;
   document.querySelector(".wind").innerHTML = `Wind speed: ${speed}mph`;
+  document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
 }
 
 let button = document.querySelector("#current");
 button.addEventListener("click", getLocation, showTemp);
+
+function tglC() {
+  // prettier-ignore
+  let c = document.getElementById("temp-celsius").style.display = "block";
+  let f = (document.getElementById("temp").style.display = "none");
+}
+function tglF() {
+  let c = (document.getElementById("temp-celsius").style.display = "none");
+  let f = (document.getElementById("temp").style.display = "block");
+}
+
+let cT = document.querySelector("#celsius");
+cT.addEventListener("click", tglC);
+let fT = document.querySelector("#fahrenheit");
+fT = fT.addEventListener("click", tglF);
